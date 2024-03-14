489a64824862e5b26bde0ac990a9db1087274b16
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-14 15:39:25 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,12 @@
+public class StringLength {
+    public static void main(String[] args) {
+        // Define a string
+        String str = "Hello, World!";
+        
+        // Find the length of the string
+        int length = str.length();
+        
+        // Print the length of the string
+        System.out.println("Length of the string: " + length);
+    }
+}
