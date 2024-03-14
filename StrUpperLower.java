a5e1e3c8badf045625df3496035157002d9e9069
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-14 20:01:17 +0530

    Add files via upload

---
+++ StrUpperLower.java
@@ -0,0 +1,13 @@
+public class StrUpperLower {
+    public static void main(String[] args) {
+        String originalString = "Hello, World!";
+        
+        // Convert to uppercase
+        String upperCaseString = originalString.toUpperCase();
+        System.out.println("Uppercase: " + upperCaseString);
+        
+        // Convert to lowercase
+        String lowerCaseString = originalString.toLowerCase();
+        System.out.println("Lowercase: " + lowerCaseString);
+    }
+}
