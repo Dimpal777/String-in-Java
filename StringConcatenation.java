90acae4c10c4c2f0c6e94bf6f97c70145ffca260
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-14 15:37:45 +0530

    Add files via upload

---
+++ StringConcatenation.java
@@ -0,0 +1,14 @@
+    public class StringConcatenation {
+    public static void main(String[] args) {
+        // Define two strings
+        String str1 = "Hello";
+        String str2 = "World";
+        
+        // Concatenate the two strings using the + operator
+        String concatenatedString = str1 + ", " + str2 + "!";
+        
+        // Print the concatenated string
+        System.out.println(concatenatedString);
+    }
+ 
+}
