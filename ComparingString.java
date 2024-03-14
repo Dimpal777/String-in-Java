7314c0b30ae200e1d299ff5dc260b0c426b1f04e
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-14 18:22:04 +0530

    Add files via upload

---
+++ ComparingString.java
@@ -0,0 +1,14 @@
+/**
+ * ComparingString
+ */
+    class ComparingString {
+
+     public static void main(String[] args) {
+
+         String str1 = "Hello";
+         String str2 = "hello";
+        System.out.println(str1.equals(str2)); 
+     
+       }
+
+}
