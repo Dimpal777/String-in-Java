6c6bd4c1f9241e8660614eb78c6bfb04e8971d42
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-14 19:29:07 +0530

    Add files via upload

---
+++ StringCase.java
@@ -0,0 +1,37 @@
+/**
+ * StringCase
+ */
+public class StringCase {
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "Hello";
+        String str = "Hello, world!";
+
+
+        System.out.println(str1.compareTo(str2));
+        System.out.println(str.endsWith("world!"));
+        System.out.println(str.startsWith("Hello"));
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        System.out.println(str1.equalsIgnoreCase(str2));
+
+    } 
+}
